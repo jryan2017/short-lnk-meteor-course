eb7400d40bc543a00b91552fd29ed85b772b208a
Josh Inselberg <joshryan05+git@gmail.com>
2017-08-29 20:57:56 +0000

    Changed paragraph text on NotFound component.

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -6,7 +6,7 @@ export default () => {
         <div className="boxed-view">
             <div className="boxed-view__box">
                 <h1>Page Not Found</h1>
-                <p>Hmmm, we're unable to find that page.</p>
+                <p>Sorry, we're unable to find that page.</p>
                 <Link to="/" className="button button--link">HEAD HOME</Link>
             </div>
         </div>
